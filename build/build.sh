53c3517c7603a7eadd6bfda72ace0e74ad106aeb
Vikentsi Lapa <v_lapa@gcore.lu>
2019-01-07 22:46:12 +0300

    More about man pages

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # this script is running to create slides 
 
+file_to_render=$1
+
 dir_slides=itacademy/LinuxEssBash/
 
 make --directory=$dir_slides clean
 cd $dir_slides
 latexmk -CA
-latexmk -pdfxe ./*linux-course.tex
+
+if [ -z "$file_to_render" ] 
+then 
+   latexmk -pdfxe ./*linux-course.tex
+else
+   latexmk -pdfxe ./$(basename "$file_to_render")
+fi
